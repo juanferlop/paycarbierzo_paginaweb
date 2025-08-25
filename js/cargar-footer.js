42e3e20feef8cbd9bf335b319b9ef2284c775b27
juanferlop <juanfernandezlopez729@gmail.com>
2025-08-26 00:17:21 +0200

    Cambios en el footer. Añadidas landing pages. Añadido favicon. Añadido secciones en el index.

--- js/cargar-footer.js
+++ js/cargar-footer.js
@@ -2,7 +2,7 @@ async function cargarFooter() {
   const elemento = document.getElementById('footer');
   if (elemento) {
     try {
-      const respuesta = await fetch('footer.html');
+      const respuesta = await fetch('/footer.html');
       elemento.innerHTML = await respuesta.text();
     } catch (error) {
       console.error('Error cargando footer:', error);
--- js/cargar-header.js
+++ js/cargar-header.js
@@ -2,7 +2,7 @@ async function cargarHeader() {
   const elemento = document.getElementById('header');
   if (elemento) {
     try {
-      const respuesta = await fetch('header.html');
+      const respuesta = await fetch('/header.html');
       elemento.innerHTML = await respuesta.text();
     } catch (error) {
       console.error('Error cargando header:', error);
